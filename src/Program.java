8d31e8c699fb5800d9517245cb3df4c08472c671
TheBestMario <mariogeorgiev02@gmail.com>
2024-12-16 22:40:30 +0000

    Hello World!

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("CLI BANKING SYSTEM");
+        System.out.println("Review client accounts (1)");
+        System.out.println("Create a new account (2)");
+        System.out.println("Exit (3)");
+        System.out.print("Enter your choice, (1,2,3): ");
+
+        boolean validChoice = false;
+        while (!validChoice) {
+            try {
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Reviewing client accounts...");
+                        validChoice = true;
+                        break;
+                    case 2:
+                        System.out.println("Creating a new account...");
+                        validChoice = true;
+                        break;
+                    case 3:
+                        System.out.println("Exiting...");
+                        validChoice = true;
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input, use a number from the list given");
+            }
+        }
     }
 }
