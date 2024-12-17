88e1748e29e0ddd7401a286256d7d66f1a9f97cb
charlie knox <charlieknox704@gmail.com>
2024-12-17 17:33:56 +0000

    example router

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,5 @@
 public class Database {
 }
+
+// this class wil hold all the CRUD operations for interacting with the DB
+
--- src/Router.java
+++ src/Router.java
@@ -1,2 +1,22 @@
 public class Router {
+
+    public Router(){}
+
+    String route(String route){
+        switch (route){
+            case "/home":
+                // routes to home page, shows home page menu with options
+                break;
+
+            case "home/customers":
+                // gets the customers for the teller
+                break;
+
+            case "logout":
+                // logouts out of the system...
+                break;
+
+        }
+        return "";
+    }
 }
