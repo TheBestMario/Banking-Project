c404eda62565362ee996f22973032e9240adaed3
charlie knox <charlieknox704@gmail.com>
2024-12-18 17:18:57 +0000

    sorting out loop in login

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -31,19 +31,14 @@ public class LoginPage {
                 teller = storedTeller;
             }
         }
-        System.out.println("answers:");
-        System.out.println(username);
-        System.out.println(password);
-
 
         if (isAuthenticated) {
             System.out.println("Login successful! Welcome, " + teller.getFirstName() + " " + teller.getLastName() + ".");
-        } else {
-            System.out.println("Invalid username or password. Please try again.");
-            return display(teller, scanner);
         }
-
-
+        else {
+            System.out.println("Invalid username or password. Please try again.");
+            teller.currentDirectory = "";
+        }
 
         teller.currentDirectory = "/home";
         return teller;
