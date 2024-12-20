a4efc145ddc2679dc63c6b1eb2675e74e827ec5d
charlie knox <charlieknox704@gmail.com>
2024-12-20 17:51:32 +0000

    adding routes

--- src/main/Router.java
+++ src/main/Router.java
@@ -59,11 +59,12 @@ public class Router {
                 return currentTeller;
 
             case "home/customers/accounts/NewBusinessAccount":
-                break;
+                currentTeller = NewBusinessAccountPage.display(currentTeller, scanner);
+                return currentTeller;
 
             case "home/customers/accounts/NewPersonalAccount":
-                // personal account creation page goes here..
-                break;
+                currentTeller = NewPersonalAccountPage.display(currentTeller, scanner);
+                return currentTeller;
 
             case "logout":
                 // TODO: clear the teller object of teller specific info
@@ -74,43 +75,3 @@ public class Router {
         return currentTeller;
     }
 }
-
-
-
-/*
-
-
- case 1:
-                        System.out.println("Selecting the existing ISA Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/ISAAccount";
-                        return currentTeller;
-                    case 2:
-                        System.out.println("Selecting the existing Personal Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/PersonalAccount";
-                        return currentTeller;
-                    case 3:
-                        System.out.println("Selecting the existing Business Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/BusinessAccount";
-                        return currentTeller;
-                    case 4:
-                        System.out.println("Selecting the existing Business Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/NewISAAccount";
-                        return currentTeller;
-                    case 5:
-                        System.out.println("Selecting the existing Business Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/NewPersonalAccount";
-                        return currentTeller;
-                    case 6:
-                        System.out.println("Selecting the existing Business Account for this customer");
-                        currentTeller.currentDirectory = "home/customers/accounts/NewBusinessAccount";
-                        return currentTeller;
-                    case 7:
-                        System.out.println("Going Back");
-                        currentTeller.currentDirectory = "home/customers";
-                        return currentTeller;
-
-
-
-
-
- */
