2a501fbadde6b5601dd53689f8e7c181b82d41f8
TheBestMario <mariogeorgiev02@gmail.com>
2024-12-23 13:50:05 +0000

    TE

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -33,8 +33,6 @@ public class Customer {
         this.email = email;
     }
     //needed empty constructor for temporary info
-    public Customer() {}
-
     public Customer(String firstName, String lastName, String photoProof, String addressProof, String businessProof, LocalDate dob) {
         /* Constructor for creating a new customer,
          is used when getting details from database and creating an object out of it. */
